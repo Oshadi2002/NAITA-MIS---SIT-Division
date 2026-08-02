2a432a4a3e60499f7a1032fff670c8fdcd9321d7
Yazi0 <yasiru01nimsara@gmail.com>
2026-08-02 11:13:57 +0530

    Trigger frontend build update

--- Frontend/client/src/App.tsx
+++ Frontend/client/src/App.tsx
@@ -1,3 +1,4 @@
+// Trigger fresh Vercel Frontend build - 2026-08-02
 import { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
